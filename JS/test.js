e88b0a92e603f93c4d53c3c23e876aa3c7245407
Oleg Kozina <olegkozina@Olegs-MacBook-Pro.local>
2024-02-08 00:02:37 -0700

    events and local storage

--- JS/test.js
+++ JS/test.js
@@ -1,32 +1,86 @@
-// const arr = [4, 6, 1, 8, 7 ]
-// arr.sort();
-// console.log(arr);
+const number = 12;
+localStorage.setItem('number', number.toString())
+console.log(localStorage.getItem('number'))
 
-let field = document.querySelector('.test-list')
-let li = Array.from(field.children);
+const userForm = document.getElementById('contact-form')
 
-let ar = [];
+const FormData = {};
+userForm.addEventListener('input', function (event){
+    FormData[event.target.name] = event.target.value;
+    localStorage.setItem('userData', JSON.stringify(FormData));
+})
+const object = JSON.parse(localStorage.getItem('userData'))
+console.log(object);
 
-for (let i of li){
-    const first = i.firstElementChild;
 
-    const x = Number(first.textContent);
+
+
+
+// const user = {
+//     name: 'Igor',
+//     age: 21,
+//     country: 'Denmark'
+// }
+
+// localStorage.setItem('user', JSON.stringify(user))
+// const raw = localStorage.getItem('user')
+// const object = JSON.parse(localStorage.getItem('user'));
+// console.log(object);
+// object.name = userName.value;
+// object.age = prompt("enter your age");
+// object.country = prompt("enter your country");
+// console.log(object);
+
+
+
+// localStorage.setItem('name', 'Kristina');
+
+
+
+// const txtItem = document.querySelector('.textarea__item');
+// let limit = txtItem.getAttribute('maxlength')
+// // console.log(limit)
+// let counter = document.querySelector('.area_counter span');
+
+// counter.innerHTML = limit;
+
+// txtItem.addEventListener('keydown', setCounter)
+
+
+// function setCounter(){
+// const result = limit - txtItem.value.length;
+// counter.innerHTML = result;
+// }
+
+
+// function openh1(){
+//     square.innerHTML = 'ttterer';
   
-    ar.push(x);
-}
-console.log(ar);
-ar.sort();
-console.log(ar);
+    
+// }
 
-function compareNumbers(a, b){
-    return a-b;
-}
-ar.sort(compareNumbers);
-console.log(ar);
 
-function uncompareNumbers(a, b){
-    return b-a;
-}
-ar.sort(uncompareNumbers);
-console.log(ar);
-li.sort((a,b)=> b-a);
+// function removeel(){
+//     txtarea_div.toggleAttribute('hidden');
+// }
+// submit.addEventListener('click', openh1)
+// submit.addEventListener('keydown', removeel)
+
+
+
+
+
+
+// button.addEventListener ('mouseover', func);
+
+//!!!
+// body.addEventListener('click', function (ev){
+//     if(ev.target.closest('.button')){
+//         func();
+//     }
+// })
+
+// submit.addEventListener('click', function(event){
+//     console.log('ttterer')
+//     event.preventDefault();
+// })
