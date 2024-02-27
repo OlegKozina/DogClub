2e88eb76c76de35bfa7ce50e7119ba6e68b57b0e
Oleg Kozina <olegkozina@Olegs-MacBook-Pro.local>
2024-02-27 01:00:19 -0700

    fixed mobile menu

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,14 +1,11 @@
 const mobileOpen = document.querySelector('.mobile-button-open');
-const mobileClose = document.querySelector('.mobile-btn-close');
+const mobileClose = document.querySelectorAll('.mobile-btn-close');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
-const disableScroll = () =>
-  document.body.classList.toggle('is-scroll-disabled');
+
 
 mobileOpen.addEventListener('click', toggleMenu);
-mobileClose.addEventListener('click', toggleMenu);
-mobileOpen.addEventListener('click', disableScroll);
-mobileClose.addEventListener('click', disableScroll);
+mobileClose.forEach((el)=>el.addEventListener('click', toggleMenu));
